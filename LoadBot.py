2ad35ba22d1d9f74a8dcf1cff6d31dbc6699afd2
WUI YANG TAN <wuiyang@live.com>
2020-06-07 01:53:27 +0800

    Update to separate code from bot login

---
+++ LoadBot.py
@@ -0,0 +1,8 @@
+from InstagramDownloader import Login
+
+username = os.environ['INSTA_USERNAME']
+password = os.environ['INSTA_PASSWORD']
+
+print("startup")
+Login(username, password)
+print("completed")
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python InstagramDownloader.py
+bot: python InstagramDownloader.py
