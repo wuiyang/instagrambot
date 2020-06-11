64fc9d39d5fe767421171dd2cf9e3f2863cfd69e
WUI YANG TAN <wuiyang@live.com>
2020-06-11 14:30:11 +0800

    added admin config var

--- LoadBot.py
+++ LoadBot.py
@@ -3,7 +3,9 @@ from InstagramDownloader import Login
 
 username = os.environ['INSTA_USERNAME']
 password = os.environ['INSTA_PASSWORD']
-admins = [username] # include admin here
+admins = []
+if 'INSTA_ADMINS' in os.environ:
+    admins = os.environ['INSTA_ADMINS'].split(" ")
 promote_message = "This bot is being run by u/floofygroup, follow them for updates"
 
 Login(username, password, admins, promote_message)
